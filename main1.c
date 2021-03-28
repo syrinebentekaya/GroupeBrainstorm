3b73c6d4b673c1c7f6f3713976b414d4ce01956f
syrinebentekaya <syrinebentekaya4@gmail.com>
2021-03-28 15:30:28 -0700

    ajout

---
+++ main1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <jeu.h>
+
+SDL_Rect position_p;
+position_p.x=100;
+position_p.y=400;
+
+SDL_Event event;
+
+int main()
+{
+
+    return 0;
+
+}
